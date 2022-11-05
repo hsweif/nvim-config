45a5b2f4eaa92cf802f7e11f5dbd71e1adcd54a2
hsweif <alex93179@gmail.com>
2022-11-05 22:06:15 +0800

    feat: set special indent for ts files

---
+++ lua/configs/languages.lua
@@ -0,0 +1,15 @@
+local M = {}
+
+function M.config()
+    vim.api.nvim_create_autocmd('FileType', {
+        pattern = { "typescript" },
+        callback = function(_)
+            -- Do something based on the value of ft
+            vim.opt.tabstop     = 2
+            vim.opt.shiftwidth  = 2
+            vim.opt.softtabstop = 2
+        end
+    })
+end
+
+return M
