3ce955c51d0b86c8a6e5fcb2763b8702e5aa4c03
hsweif <alex93179@gmail.com>
2023-04-18 22:32:57 +0800

    fix: update deprecated configs

--- lua/configs/autocomplete.lua
+++ lua/configs/autocomplete.lua
@@ -107,7 +107,7 @@ function M.config()
     -- List of all pre-configured LSP servers:
     -- github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
     require 'lspconfig'.gopls.setup {}
-    local servers = { 'clangd', 'rust_analyzer', 'pylsp', 'sumneko_lua', 'tsserver', 'gopls' }
+    local servers = { 'clangd', 'rust_analyzer', 'pylsp', 'lua_ls', 'tsserver', 'gopls' }
     for _, lsp in pairs(servers) do
         require('lspconfig')[lsp].setup {
             on_attach = on_attach
@@ -132,7 +132,7 @@ function M.config()
     local saga = require 'lspsaga'
 
     -- use default config
-    saga.init_lsp_saga({
+    saga.setup({
         -- Options with default value
         -- "single" | "double" | "rounded" | "bold" | "plus"
         border_style = "bold",
